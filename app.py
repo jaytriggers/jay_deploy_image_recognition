53fc930e6b560b1e949f4233d0fff8e14a973789
MacBook Air <macbookair@MacBooks-MacBook-Air.local>
2025-09-27 10:38:50 +0100

    update to script

--- app.py
+++ app.py
@@ -1,7 +1,33 @@
 import gradio as gr
+from fastai.vision.all import *
 
-def greet(name):
-    return "Hello " + name + "!!"
+__all__ = ['learn','categories','classify_image','image','label','examples','demo','greet'] 
 
-demo = gr.Interface(fn=greet, inputs="text", outputs="text")
+learn = load_learner('bear_image_detection_model.pkl')
+
+category = ('Blackbear','Grizzlybear','Teddybear')
+def classify_images(img_path):
+    im = PILImage.create(img_path)
+    pred_class, pred_idx, probs = learn.predict(im)
+    return dict(zip(category,map(float,probs)))
+
+
+image = gr.Image(type="pil", label="Input Image")   # For input images
+label = gr.Label(label="Prediction")               # For output labels
+
+examples = [
+    "data/bears/grizzlybear/Image_1.jpg",
+    "data/bears/teddybear/Image_1.jpg",
+    "data/bears/blackbear/Image_1.jpg",
+    "data/cats/Image_1.jpg"
+]
+
+demo = gr.Interface(
+    fn=classify_images,   # your prediction function
+    inputs=image,
+    outputs=label,
+    examples=examples
+)
+
 demo.launch()
+
