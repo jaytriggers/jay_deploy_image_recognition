55c045b7d8cfc72b67947a29812a77eb2174f0e1
MacBook Air <macbookair@MacBooks-MacBook-Air.local>
2025-09-27 13:57:05 +0100

    updated app.py

--- app.py
+++ app.py
@@ -1,19 +1,22 @@
 import gradio as gr
 from fastai.vision.all import *
 
-__all__ = ['learn','categories','classify_image','image','label','examples','demo','greet'] 
+__all__ = ['learn','category','classify_images','image','label','examples','demo'import gradio as gr
+from fastai.vision.all import *
+
+__all__ = ['learn','categories','classify_images','image','label','examples','demo']
 
 learn = load_learner('bear_image_detection_model.pkl')
 
-category = ('Blackbear','Grizzlybear','Teddybear')
+categories = ('Blackbear','Grizzlybear','Teddybear')
+
 def classify_images(img_path):
     im = PILImage.create(img_path)
     pred_class, pred_idx, probs = learn.predict(im)
-    return dict(zip(category,map(float,probs)))
+    return dict(zip(categories, map(float, probs)))
 
-
-image = gr.Image(type="pil", label="Input Image")   # For input images
-label = gr.Label(label="Prediction")               # For output labels
+image = gr.Image(type="pil", label="Input Image")
+label = gr.Label(label="Prediction")
 
 examples = [
     "data/bears/grizzlybear/Image_1.jpg",
@@ -22,12 +25,10 @@ examples = [
     "data/cats/Image_1.jpg"
 ]
 
-demo = gr.Interface(
-    fn=classify_images,   # your prediction function
-    inputs=image,
-    outputs=label,
-    examples=examples
-)
+demo = gr.Interface(fn=classify_images,
+                    inputs=image,
+                    outputs=label,
+                    examples=examples)
 
 demo.launch()
 
